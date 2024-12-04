9b447349605deb9c67d719b6a5b9bc3708e4577f
oandris228 <106300817+oandris228@users.noreply.github.com>
2024-12-04 11:32:10 +0100

    started db

---
+++ prisma/seed.ts
@@ -0,0 +1,10 @@
+import { faker } from '@faker-js/faker'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient
+
+async function main() {
+    for (let i = 0; i < 15; i++) {
+        
+    }
+}
